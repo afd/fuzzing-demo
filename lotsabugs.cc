9e0d6177717f5328b4d32a46b7f2147c8941b28d
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-03-02 18:20:28 +0000

    Finished sanitizers part.

--- lotsabugs.cc
+++ lotsabugs.cc
@@ -2,13 +2,14 @@
 
 int main(int argc, const char** argv) {
   int* A = new int[argc];
-  for (int i = 0; i <= argc; i++) {
+  for (int i = 0; i < argc; i++) {
     A[i] = 1000000;
   }
-  int temp = A[0];
-  if (temp*temp < 0) {
-    A[0] /= (argc - 1);
+  unsigned temp = A[0];
+  if (temp*temp > 1) {
+    A[0] /= (argc);
   }
   std::cout << "Phew, made it to the end!" << std::endl;
+  delete[] A;
   return 0;
 }
