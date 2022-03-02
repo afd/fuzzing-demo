1c962fc6f7341d9fa817b4eaa6129bf8d315479b
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-03-02 17:46:32 +0000

    Ready for demo.

--- src/solver.cc
+++ src/solver.cc
@@ -1,9 +1,13 @@
 #include <functional>
 #include <iostream>
+#include <sstream>
 
 #include "src/solver.h"
 
-std::string GetToken(const std::string &input, size_t &index) {
+std::optional<std::string> GetToken(const std::string &input, size_t &index) {
+  if (index >= input.size()) {
+    return {};
+  }
   while (true) {
     char current = input[index];
     if (current != ' ' && current != '\n') {
@@ -14,6 +18,9 @@ std::string GetToken(const std::string &input, size_t &index) {
   std::string result;
   while (true) {
     // BUG: 'index' can be out of bounds
+    // if (index == input.size()) {
+    //  break;
+    //}
     char current = input[index];
     index++;
     if (current == ' ' || current == '\n') {
@@ -21,7 +28,10 @@ std::string GetToken(const std::string &input, size_t &index) {
     }
     result += current;
   }
-  return result;
+  if (result.empty()) {
+    return {};
+  }
+  return {result};
 }
 
 std::optional<SatInstance> BCP(const SatInstance &instance,
@@ -30,7 +40,12 @@ std::optional<SatInstance> BCP(const SatInstance &instance,
   std::optional<SatInstance> result = instance;
   while (true) {
     std::optional<int32_t> unit = {};
+    int num_large_clauses = 0;
     for (auto &clause : result.value().clauses) {
+      if (clause.size() > 8) {
+        num_large_clauses++;
+        assert(num_large_clauses < 5);
+      }
       if (clause.size() == 1) {
         unit = {*clause.begin()};
         break;
@@ -59,6 +74,14 @@ SolveRecursive(SatInstance instance, std::unordered_set<int32_t> assignment,
   if (!simplified_instance.has_value()) {
     return {};
   }
+  // BUG (performance): we should cease solving if there are no more clauses
+  // to satisfy
+  // if (simplified_instance->clauses.empty()) {
+  //  for (auto var : unassigned) {
+  //    assignment.insert(var);
+  //  }
+  //  return {assignment};
+  //}
   if (unassigned.empty()) {
     return {assignment};
   }
@@ -112,8 +135,8 @@ SolveNaiveRecursive(SatInstance instance,
     assignment.erase(decision_var);
   }
   {
-    // Bug 1: should be -decision_var
-    // Bug 2: should call SolveNaiveRecursive
+    // BUG: should be -decision_var
+    // BUG: should call SolveNaiveRecursive
     assignment.insert(decision_var);
     return SolveRecursive(instance.Assert(-decision_var).value(), assignment,
                           unassigned);
@@ -134,6 +157,10 @@ bool IsSatisfyingAssignment(const SatInstance &instance,
                             const std::unordered_set<int32_t> &assignment) {
   std::optional<SatInstance> remaining{instance};
   for (auto literal : assignment) {
+    // BUG (injected)
+    if (literal == 10) {
+      return false;
+    }
     remaining = remaining.value().Assert(literal);
     if (!remaining.has_value()) {
       return false;
@@ -145,28 +172,83 @@ bool IsSatisfyingAssignment(const SatInstance &instance,
 std::optional<SatInstance>
 Parse(const std::string &input,
       const std::function<void(const std::string &)> &error_consumer) {
+  // BUG: try-catch required because std::stroi may throw an exception.
+  // try {
   size_t index = 0;
-  if (GetToken(input, index) != "p") {
+  auto tok = GetToken(input, index);
+  if (!tok.has_value()) {
+    error_consumer("Unexpected end of input");
+    return {};
+  }
+  if (tok != "p") {
     error_consumer("Expected p as first token of DIMACS CNF formula");
     return {};
   }
-  if (GetToken(input, index) != "cnf") {
+  tok = GetToken(input, index);
+  if (!tok.has_value()) {
+    error_consumer("Unexpected end of input");
+    return {};
+  }
+  if (tok != "cnf") {
     error_consumer("Expected cnf as second token of DIMACS CNF formula");
     return {};
   }
-  size_t num_vars = atoi(GetToken(input, index).c_str());
-  size_t num_clauses = atoi(GetToken(input, index).c_str());
+  tok = GetToken(input, index);
+  if (!tok.has_value()) {
+    error_consumer("Unexpected end of input");
+    return {};
+  }
+  // BUG: a negative number will lead to a HUGE number of variables
+  size_t num_vars = std::stoi(tok.value());
+  tok = GetToken(input, index);
+  if (!tok.has_value()) {
+    error_consumer("Unexpected end of input");
+    return {};
+  }
+  size_t num_clauses = std::stoi(tok.value());
   std::vector<std::unordered_set<int32_t>> clauses;
   for (size_t i = 0; i < num_clauses; i++) {
     std::unordered_set<int32_t> clause;
     while (true) {
-      int32_t literal = atoi(GetToken(input, index).c_str());
+      tok = GetToken(input, index);
+      // BUG: need to check whether token exists
+      // if (!tok.has_value()) {
+      //  error_consumer("Unexpected end of input");
+      //  return {};
+      //}
+      int32_t literal = std::stoi(tok.value());
       if (literal == 0) {
         break;
       }
+      // BUG: we only want literals that are in range
+      // if (literal > static_cast<int>(num_vars) ||
+      //    literal < -static_cast<int>(num_vars)) {
+      //  std::stringstream stringstream;
+      //  stringstream << "Literal " << literal << " out of range";
+      //  error_consumer(stringstream.str());
+      //  return {};
+      //}
       clause.insert(literal);
     }
+    // BUG: empty clauses are not allowed
+    // if (clause.empty()) {
+    //  error_consumer("Empty clauses are not allowed");
+    //  return {};
+    //}
     clauses.push_back(clause);
   }
+  // BUG: nicer if we check there was not excess input
+  // tok = GetToken(input, index);
+  // if (tok.has_value()) {
+  //  error_consumer("Too much input");
+  //  return {};
+  //}
   return {{num_vars, clauses}};
+  //} catch (const std::invalid_argument&) {
+  //  error_consumer("Parse error");
+  //  return {};
+  //} catch (const std::out_of_range&) {
+  //  error_consumer("Parse error");
+  //  return {};
+  //}
 }
